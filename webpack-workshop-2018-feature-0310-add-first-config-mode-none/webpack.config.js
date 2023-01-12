e6e22c6c5aa9697c9409094ef1a63cf0d60094c1
jy-whatthetech <james.yeh1@gmail.com>
2023-01-11 23:59:08 -0800

    log the env mode value

--- webpack-workshop-2018-feature-0310-add-first-config-mode-none/webpack.config.js
+++ webpack-workshop-2018-feature-0310-add-first-config-mode-none/webpack.config.js
@@ -1,5 +1,9 @@
-module.exports = () => {
-  output: {
-    filename: "bundle.js"
+module.exports = env => {
+  console.log(env);
+  return {
+    mode: env.mode,
+    output: {
+      filename: "bundle.js"
+    }
   }
 };
