7b6c7628e8ca2b93b6c3d116355d37aadfa86fc1
jy-whatthetech <james.yeh1@gmail.com>
2023-01-16 22:59:25 -0800

    use webpack-dev-server to do hot-reload
    create an actual button in the html page

--- webpack-workshop-2018-feature-0310-add-first-config-mode-none/src/index.js
+++ webpack-workshop-2018-feature-0310-add-first-config-mode-none/src/index.js
@@ -3,6 +3,9 @@ import { top, bottom } from "./footer";
 import makeButton from "./button";
 import { makeColorStyle } from "./button-styles";
 
+const button = makeButton("Yay! A Button!");
+document.body.appendChild(button);
+
 console.log(
   nav(),
   top,
