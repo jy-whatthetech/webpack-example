6eda961c6948f98464b0292d7a3c463d638b2fa7
jy-whatthetech <james.yeh1@gmail.com>
2023-01-19 08:29:08 -0800

    use webpack-merge to merge env configs

--- webpack-workshop-2018-feature-0310-add-first-config-mode-none/webpack.config.js
+++ webpack-workshop-2018-feature-0310-add-first-config-mode-none/webpack.config.js
@@ -1,9 +1,11 @@
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { merge } = require("webpack-merge");
+
 const modeConfig = env => require(`./build-utils/webpack.${env}`)(env);
 
 module.exports = ({ development, presets } = { development: false, presets: [] }) => {
-  return {
+  return merge({
     mode: development ? "development" : "production",
     output: {
       filename: "bundle.js"
@@ -11,5 +13,7 @@ module.exports = ({ development, presets } = { development: false, presets: [] }
     plugins: [
       new HtmlWebpackPlugin(), new webpack.ProgressPlugin()
     ]
-  }
+  },
+    modeConfig(development ? "development" : "production")
+  );
 };
