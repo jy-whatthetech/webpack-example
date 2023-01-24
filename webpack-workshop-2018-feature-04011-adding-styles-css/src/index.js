17f00bd2717c8c8d23932a6ae474830c34a281ba
jy-whatthetech <james.yeh1@gmail.com>
2023-01-24 15:59:08 -0800

    use url and file-loader to load images

---
+++ webpack-workshop-2018-feature-04011-adding-styles-css/src/index.js
@@ -0,0 +1,15 @@
+import nav from "./nav";
+import { footer } from "./footer";
+import makeButton from "./button";
+import { makeColorStyle } from "./button-styles";
+import image from "./webpack-logo.jpg";
+
+console.log(image);
+
+import css from "./footer.css";
+import buttonStyles from "./button.css";
+
+const button = makeButton("Yay! A Button!");
+button.style = makeColorStyle("cyan");
+document.body.appendChild(button);
+document.body.appendChild(footer);
