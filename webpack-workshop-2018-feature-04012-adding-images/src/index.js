71676663ffbbd354d49a80dfac670fff8815e2c1
jy-whatthetech <james.yeh1@gmail.com>
2023-02-02 08:30:19 -0800

    load footer only when button is clicked
    example of code splitting

--- webpack-workshop-2018-feature-04012-adding-images/src/index.js
+++ webpack-workshop-2018-feature-04012-adding-images/src/index.js
@@ -1,5 +1,5 @@
 import nav from "./nav";
-import { footer } from "./footer";
+// import { footer } from "./footer";
 import makeButton from "./button";
 import { makeColorStyle } from "./button-styles";
 import makeImage from "./image";
@@ -8,10 +8,18 @@ import css from "./footer.css";
 import buttonStyles from "./button.css";
 // import Foo from "./foo.ts";
 
+const loadFooter = () => import("./footer");
+
 const image = makeImage(imageUrl);
 const button = makeButton("Yay! A Button!");
 button.style = makeColorStyle("cyan");
 
+button.addEventListener("click", e => {
+    loadFooter().then(m=> {
+        document.body.appendChild(m.footer);
+    })
+});
+
 document.body.appendChild(button);
 document.body.appendChild(image);
-document.body.appendChild(footer);
+
