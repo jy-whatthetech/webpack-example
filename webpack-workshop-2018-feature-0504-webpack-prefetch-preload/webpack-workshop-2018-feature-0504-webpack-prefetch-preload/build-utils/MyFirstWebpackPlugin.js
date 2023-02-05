499f224033fd9bf2d73dfe32736a47e82f8af643
jy-whatthetech <james.yeh1@gmail.com>
2023-02-04 22:20:48 -0800

    list asset names with plugin

--- webpack-workshop-2018-feature-0504-webpack-prefetch-preload/webpack-workshop-2018-feature-0504-webpack-prefetch-preload/build-utils/MyFirstWebpackPlugin.js
+++ webpack-workshop-2018-feature-0504-webpack-prefetch-preload/webpack-workshop-2018-feature-0504-webpack-prefetch-preload/build-utils/MyFirstWebpackPlugin.js
@@ -2,6 +2,7 @@ class MyFirstWebpackPlugin {
     apply(compiler) {
         compiler.hooks.done.tapAsync("MyFirstWebpackPlugin", (stats, cb)=>{
             console.log(stats);
+            debugger;
             cb();
         })
     }
