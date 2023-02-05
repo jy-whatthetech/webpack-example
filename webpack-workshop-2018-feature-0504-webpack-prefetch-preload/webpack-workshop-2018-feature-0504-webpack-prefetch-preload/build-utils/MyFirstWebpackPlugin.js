546d87faccf46ef2a4e3121d37ed04323e3ccafe
jy-whatthetech <james.yeh1@gmail.com>
2023-02-05 14:52:26 -0800

    hook into app right before seal happens
    seal phase is the phase right before where page renders

--- webpack-workshop-2018-feature-0504-webpack-prefetch-preload/webpack-workshop-2018-feature-0504-webpack-prefetch-preload/build-utils/MyFirstWebpackPlugin.js
+++ webpack-workshop-2018-feature-0504-webpack-prefetch-preload/webpack-workshop-2018-feature-0504-webpack-prefetch-preload/build-utils/MyFirstWebpackPlugin.js
@@ -2,11 +2,19 @@ class MyFirstWebpackPlugin {
     apply(compiler) {
         compiler.hooks.done.tapAsync("MyFirstWebpackPlugin", (stats, cb)=>{
             console.log(stats);
+            const assetNames = [];
             for (let assetName in stats.compilation.assets) {
                 assetNames.push(assetName);
             }
-            console.log(assetNames.join("\n"));
+            // console.log(assetNames.join("\n"));
             cb();
+        });
+
+        compiler.hooks.compilation.tap("MyFirstWebpackPlugin", (compilation, params) => {
+            const thisCompilationIWantToInspect = compilation;
+            compilation.hooks.seal.tap("MyFirstWebpackPlugin", () => {
+                console.log(thisCompilationIWantToInspect);
+            });
         })
     }
 }
