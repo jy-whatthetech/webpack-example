d90f7ea3d37c9709733890715813418136100a72
jy-whatthetech <james.yeh1@gmail.com>
2023-02-09 19:59:51 -0800

    clone code-splitting-large-libs example

---
+++ webpack-workshop-2018-feature-0501-code-splitting-large-libs/src/index.js
@@ -0,0 +1,31 @@
+import nav from "./nav";
+// import * as GSAP from "gsap";
+const getGSAP = () => import("gsap");
+// import { footer } from "./footer";
+const getFooter = () => import("./footer");
+const getLodashUniq = () => import("lodash-es/uniq");
+import makeButton from "./button";
+import { makeColorStyle } from "./button-styles";
+import makeImage from "./image";
+import imageUrl from "./webpack-logo.jpg";
+import css from "./footer.css";
+import buttonStyles from "./button.css";
+
+const image = makeImage(imageUrl);
+const button = makeButton("Yay! A Button!");
+button.style = makeColorStyle("cyan");
+
+document.body.appendChild(button);
+
+button.addEventListener("click", e => {
+    getFooter().then(({footer}) => {
+        document.body.appendChild(footerModule.footer);
+    });
+
+    getGSAP().then(gsap => {
+        console.log(gsap);
+    });
+});
+
+document.body.appendChild(image);
+
