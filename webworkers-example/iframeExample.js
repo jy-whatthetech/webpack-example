95a8ddd6d3ad5f8da4f76b7314a79048e1556572
jy-whatthetech <james.yeh1@gmail.com>
2023-02-20 22:55:15 -0800

    add reference to sse tpl to allow POST

--- webworkers-example/iframeExample.js
+++ webworkers-example/iframeExample.js
@@ -1,1 +1,15 @@
 // example of using iframe postMessage to commuicate between browser tabs
+
+// to make an sse connection with POST and headers:
+// use https://github.com/mpetazzoni/sse.js
+
+/*
+headers to include:
+    Content-Type
+    IDS-SESSION-ID
+
+orgId and userId are in the url
+
+var source = new SSE(url, {headers: {'Content-Type': 'application/json'},
+                           method: 'POST'});
+*/
